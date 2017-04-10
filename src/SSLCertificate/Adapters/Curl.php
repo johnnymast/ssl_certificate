ece92fe45686dfabc49bbd7632e180d62cba1532
Johnny Mast <mastjohnny@gmail.com>
2017-04-10 20:27:32 +0200

    Added the basics for curl support for downloading the ssl certificate.

--- src/SSLCertificate/Adapters/Curl.php
+++ src/SSLCertificate/Adapters/Curl.php
@@ -4,7 +4,6 @@ namespace JM\Validators\SSLCertificate\Adapters;
 
 class Curl extends AdapterAbstract implements AdapterInterface
 {
-
     /**
      * Interact with the given source and
      * grab the certificate.
@@ -17,22 +16,24 @@ class Curl extends AdapterAbstract implements AdapterInterface
     {
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$host);
+        curl_setopt($ch, CURLOPT_URL, $host);
         curl_setopt($ch, CURLOPT_PORT, $port);
-  //      curl_setopt($ch, CURLOPT_CERTINFO, 1);
- //       curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_CERTINFO, 1);
         curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_NOBODY, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         $result = curl_exec($ch);
 
         $cert = curl_getinfo($ch, CURLINFO_CERTINFO);
 
         if (is_array($cert) == true and count($cert) > 0) {
             $cert = $cert[0]['Cert'];
+
             return openssl_x509_parse($cert);
         }
+
         return null;
     }
 }
